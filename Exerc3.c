66aed0f850234c840cbd7f48be0f195378390225
Your Name <gabrilgasparotto45@gmail.com>
2025-02-05 22:28:29 -0300

    Algoritmo que faz a leitura de uma matriz 2x2 e imprime a matriz transposta

---
+++ Exerc3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int mat[2][2];
+    
+    // Algoritmo que faz a leitura de uma matriz 2x2 e imprime a matriz transposta
+    printf("Digite os valores da matriz 2x2\n");
+    // This nested loop will iterate over the matrix and ask for the user to input the values
+    for (int i=0; i<2; i++){
+        for (int j=0; j<2; j++){
+            
+            printf("Digite o valor da posicao [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+            printf("[%d][%d] : %d\n",i, j, mat[i][j]);
+        }
+        
+    }
+
+    // traverse the matrix and print the values
+    for (int j=0; j<2; j++){
+        for (int i=0; i<2; i++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
