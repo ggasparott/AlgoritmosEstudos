7e78b3d3f7421078f000d0d423bced0d6669a1db
Your Name <gabrilgasparotto45@gmail.com>
2025-02-05 22:29:29 -0300

    Algoritmo que encontra o maior numero de um vetor

---
+++ Exerc5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Algoritmo que encontra o maior numero de um vetor
+    int num[10], maior;
+
+    
+    for (int i = 0; i < 10; i++){
+        printf("Digite o %d numero: ", i + 1);
+        scanf("%d", &num[i]);
+        
+    }
+    maior = num[0];
+    
+    for(int i = 0; i < 10; i++){
+        if(num[i] > maior){
+            maior = num[i];
+        }
+    }
+    printf("O maior numero digitado foi: %d\n", maior);
+}
